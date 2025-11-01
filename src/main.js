79ff19e10c3f58f9ea80e40cdbba1187a43a5f1f
smtechien <sulthonrahmatullah@gmail.com>
2025-11-02 00:01:20 +0800

    Memperbaiki struktur folder vendor/ dan menyesuaikan file import di main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // currently empty
 //
 
-import { Camera, Group, Scene } from "https://cdn.skypack.dev/three@0.132.2";
+import { Camera, Group, Scene } from "three";
 
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js?module";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js?module";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
