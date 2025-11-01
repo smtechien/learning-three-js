4ec47ec58abd867ad19e610cac93aed84984f9c8
smtechien <sulthonrahmatullah@gmail.com>
2025-11-01 23:56:59 +0800

    Menyusun struktur dasar three.js app

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// currently empty
+//
+
+import { Camera, Group, Scene } from "https://cdn.skypack.dev/three@0.132.2";
+
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js?module";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js?module";
