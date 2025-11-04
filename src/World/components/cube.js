2f3b1e5dcbc08b8ed5aadc275f0e3fa9d674895a
smtechien <sulthonrahmatullah@gmail.com>
2025-11-04 23:12:54 +0800

    Merubah material cube, merotasi cube, dan menambahan pencahayaan

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,14 +1,16 @@
 import {
   BoxBufferGeometry,
   Mesh,
-  MeshBasicMaterial,
+  MeshStandardMaterial,
 } from "https://cdn.skypack.dev/three@0.132.2";
 
 function createCube() {
   const geometry = new BoxBufferGeometry(2, 2, 2);
-  const material = new MeshBasicMaterial();
+  const material = new MeshStandardMaterial({ color: "purple" });
 
   const cube = new Mesh(geometry, material);
+
+  cube.rotation.set(-0.5, -0.1, 0.8);
 
   return cube;
 }
---
+++ src/World/components/lights.js
@@ -0,0 +1,11 @@
+import { DirectionalLight } from "https://cdn.skypack.dev/three@0.132.2";
+
+function createLights() {
+  const light = new DirectionalLight("white", 8);
+
+  // pindah  posisi cahaya ke kanan, atas, dan lebih kedepan.
+  light.position.set(10, 10, 10);
+  return light;
+}
+
+export { createLights };
